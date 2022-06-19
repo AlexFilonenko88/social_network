430cbf79d0130a8fefaa7cc1482e061027b28287
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-06-19 17:56:30 +0300

    add sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://avatars.mds.yandex.net/i?id=3b84ba0a875426c558f8592865b5fa51-5296782-images-thumbs&n=13'/>
       </header>
+      <nav className='nav'>
+        <div>
+          Profile
+        </div>
+        <div>
+          Messages
+        </div>
+      </nav>
+      <div className='content'>
+        Main content
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
