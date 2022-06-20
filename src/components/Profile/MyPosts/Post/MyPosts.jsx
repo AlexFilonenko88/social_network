bf3d8c2dccd02f196d8da64eaaf0b383dacdc1a4
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-06-20 11:15:46 +0300

    fix tag img

---
+++ src/components/Profile/MyPosts/Post/MyPosts.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import MP from "./MyPosts.module.css";
+
+const MyPosts = () => {
+  return (
+    <div className={MP.content}>
+      <div>
+        My posts
+        <div>
+          <textarea></textarea>
+          <button>Add post</button>
+          <button>Remove</button>
+        </div>
+      </div>
+      <div className={MP.posts}>
+        <div className={MP.item}>
+        
+        Post 1</div><img src="https://w-dog.ru/wallpapers/1/59/538931223085784/smajl-naushniki-3-j.jpg" alt="image" />
+        <div className={MP.item}>Post 2</div>
+        <div className={MP.item}>Post 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default MyPosts;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ import P from "./Post.module.css";
 const Post = () => {
   return (
       <div className={P.item}>
-        <img src="https://w-dog.ru/wallpapers/1/59/538931223085784/smajl-naushniki-3-j.jpg" />
+        <img src="https://w-dog.ru/wallpapers/1/59/538931223085784/smajl-naushniki-3-j.jpg" alt="image_post" />
         Post 1
         <div>
           <span>like</span>
