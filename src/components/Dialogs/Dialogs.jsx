715e983d96fed17167cce14d37a302ed1e8b1bf4
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-06-22 18:46:50 +0300

    end lesson 25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import D from "./Dialogs.module.css";
 
-const DialogsItem = ({name, id}) => {
-    let path = "/dialogs/" + id;
+const DialogsItem = ({ name, id }) => {
+  let path = "/dialogs/" + id;
 
   return (
     <div className={D.dialog + " " + D.active}>
@@ -12,28 +12,51 @@ const DialogsItem = ({name, id}) => {
   );
 };
 
-const Message = ({message}) => {
-    return (
-        <div className={D.message}>{message}</div>
-    )
-}
+const Message = ({ message }) => {
+  return <div className={D.message}>{message}</div>;
+};
 
 const Dialogs = (props) => {
+  let dialogs = [
+    { id: 1, name: "Alex" },
+    { id: 2, name: "Andrey" },
+    { id: 3, name: "Max" },
+    { id: 4, name: "Sveta" },
+    { id: 5, name: "Valera" },
+    { id: 6, name: "Viktor" },
+  ];
+
+  let message = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How is your WWW?" },
+    { id: 3, message: "Yo!" },
+    { id: 4, message: "Yo!" },
+  ];
+
+  let dialogsElements = dialogs.map((d) => (
+    <DialogsItem name={d.name} id={d.id} />
+  ));
+
+  let messagesElements = message.map((m) => (
+    <Message message={m.message} id={m.id} />
+  ));
+
   return (
     <div className={D.dialogs}>
       <div className={D.dialogs_items}>
-
-        <DialogsItem name="Alex" id="1"/>
-        <DialogsItem name="Andrey" id="2"/>
-        <DialogsItem name="Max" id="3"/>
-        <DialogsItem name="Sveta" id="4"/>
-        <DialogsItem name="Valera" id="5"/>
-        <DialogsItem name="Viktor" id="6"/>
+        {dialogsElements}
+        {/* <DialogsItem name="Alex" id="1" />
+        <DialogsItem name="Andrey" id="2" />
+        <DialogsItem name="Max" id="3" />
+        <DialogsItem name="Sveta" id="4" />
+        <DialogsItem name="Valera" id="5" />
+        <DialogsItem name="Viktor" id="6" /> */}
       </div>
       <div className={D.messages}>
-        <Message message="Hi" />
+        {messagesElements}
+        {/* <Message message="Hi" />
         <Message message="How is your WWW?" />
-        <Message message="Yo!" />
+        <Message message="Yo!" /> */}
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,24 +3,30 @@ import MP from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+  let posts = [
+    { id: 1, message: "Hi, how are you?", like: 15 },
+    { id: 2, message: "Its my first post", like: 11 },
+  ];
+
+  let postsElement = posts.map((p) => (
+    <Post message={p.message} like={p.like} />
+  ));
+
   return (
     <div className={MP.posts_block}>
-        <h3>My posts</h3>
+      <h3>My posts</h3>
+      <div>
         <div>
-          <div>
-            <textarea></textarea>
-          </div>
-          <div>
-            <button>Add post</button>
-          </div>
-          <div>
-            <button>Remove</button>
-          </div>
+          <textarea></textarea>
         </div>
-      <div className={MP.posts}>
-        <Post message='Hi, how are you?' like="15"/>
-        <Post message="Its my first post" like="10"/>
+        <div>
+          <button>Add post</button>
+        </div>
+        <div>
+          <button>Remove</button>
+        </div>
       </div>
+      <div className={MP.posts}>{postsElement}</div>
     </div>
   );
 };
